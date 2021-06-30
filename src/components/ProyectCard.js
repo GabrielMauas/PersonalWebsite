0175f52047c9df70146c1c9d6c228fff95f32ce0
Gabriel Mauas <gabymauas02@gmail.com>
2021-06-30 14:04:05 -0300

    Corrected a mistake on the card links

--- src/components/ProyectCard.js
+++ src/components/ProyectCard.js
@@ -11,8 +11,8 @@ const ProyectCard = (props) => {
                 <h3 className='pr-title'>{title}</h3>
                 <p className='pr-descr'>{text}</p>
                 <div className='pr-links'>
-                    <a href={ghLink} target="_blank" rel="noreferrer" className='bx bxl-github pr-link'></a>
-                    <a href={link} target="_blank" rel="noreferrer" className='bx bx-link-external pr-link'></a>
+                    <a href={ghLink} target="_blank" rel="noreferrer"><i className='bx bxl-github pr-link'></i></a>
+                    <a href={link} target="_blank" rel="noreferrer"><i className='bx bx-link-external pr-link'></i></a>
                 </div>
             </div>
         </div>
